5f9c04f88de44a825460804ff2de607a225d66c8
alhanger <hanger129@gmail.com>
2015-12-02 00:47:47 -0500

    Create maze generator with start and end points

---
+++ src/com/theironyard/Room.java
@@ -0,0 +1,19 @@
+package com.theironyard;
+
+/**
+ * Created by alhanger on 12/1/15.
+ */
+public class Room {
+    int row;
+    int col;
+    boolean wasVisited = false;
+    boolean hasBottom = true;
+    boolean hasRight = true;
+    boolean isStart = false;
+    boolean isEnd = false;
+
+    public Room(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+}
